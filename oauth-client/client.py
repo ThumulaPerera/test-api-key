9a25b23bd90fd8a4527904aa43090ea4237c8ab9
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2024-11-04 09:36:18 +0530

    Add OAuth client

---
+++ oauth-client/client.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+def get_access_token():
+    url = os.getenv('TOKEN_URL')
+    client_id = os.getenv('CLIENT_ID')  # Read from environment variable
+    client_secret = os.getenv('CLIENT_SECRET')
+    response = requests.post(
+        url,
+        data={"grant_type": "client_credentials"},
+        auth=(client_id, client_secret),
+    )
+    return response.json()["access_token"]
+
+access_token = get_access_token()
+print(f"access token: {access_token}")
+
+# invoke an API with the access token
+url = os.getenv('API_URL')
+response = requests.get(url, headers={"Authorization": f"Bearer {access_token}"})
+print(f"response status: {response.status_code}")
+print(f"response payload: {response.json()}")
