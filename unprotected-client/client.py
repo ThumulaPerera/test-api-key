ba8f47f64b2643ae66d4cc4043b74a1c2cac34ac
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2024-11-05 12:43:28 +0530

    Update component config

--- unprotected-client/client.py
+++ unprotected-client/client.py
@@ -4,6 +4,8 @@ import requests
 # invoke an API with the access token
 url = os.getenv('API_URL')
 print(f"api url: {url}")
+api_key = os.getenv('API_KEY')
+print(f"api key (unused): {api_key}")
 
 url += "/greeting?name=John"
 
