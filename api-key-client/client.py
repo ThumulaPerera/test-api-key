e85ab18ded2a9ba601956d44d7746fe5c75ff18b
ThumulaPerera <42399179+ThumulaPerera@users.noreply.github.com>
2024-12-03 17:15:29 +0530

    update yaml

--- api-key-client/client.py
+++ api-key-client/client.py
@@ -4,10 +4,12 @@ import requests
 # invoke an API with the access token
 url = os.getenv('API_URL')
 print(f"api url: {url}")
+api_context = os.getenv('API_CONTEXT')
+print(f"api context: {api_context}")
 api_key = os.getenv('API_KEY')
 print(f"api key: {api_key}")
 
-url += "/greeting?name=John"
+url += api_context
 
 response = requests.get(url, headers={"choreo-api-key": f"{api_key}"})
 print(f"response status: {response.status_code}")
